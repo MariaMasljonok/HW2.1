b78749ccb2c0d200652f4d6be53b411ce25838f4
Maria Masljonok <maria@onoffapp.com>
2021-01-27 13:57:59 +0200

    Initial Commit

---
+++ HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1
+//
+//  Created by Maria Masljonok on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
